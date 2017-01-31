bugfix related to forming the kernel with derivative observations in +2 dimensions + optimizing the hyperparameters with gradient observations in +2 dimensions

GPy/inference/latent_function_inference/exact_gaussian_inference.py
@@ -43,7 +43,7 @@ class ExactGaussianInference(LatentFunctionInference):
                 K = kern.Kd(X, Xd=Xd, Xdi=di, X2=X, X2d=Xd, X2di=di)
                 if (Yd is None) or not (Xd.shape == Yd.shape) or not (Xd.shape == di.shape):
                     raise AttributeError("Values and indices for derivative observations not given or they are of wrong size")
-                YYT_factor = np.array(np.bmat([[YYT_factor], [np.array([(Yd.reshape(-1))[np.nonzero(di.T.reshape(-1))]]).T]]))
+                YYT_factor = np.array(np.bmat([[YYT_factor], [np.array([(Yd.T.reshape(-1))[np.nonzero(di.T.reshape(-1))]]).T]]))
         Ky = K.copy()
         diag.add(Ky, variance+1e-8)
 
@@ -61,7 +61,6 @@ class ExactGaussianInference(LatentFunctionInference):
         dL_dK = 0.5 * (tdot(alpha) - Y.shape[1] * Wi)
 
         dL_dthetaL = likelihood.exact_inference_gradients(np.diag(dL_dK), Y_metadata)
-
         return Posterior(woodbury_chol=LW, woodbury_vector=alpha, K=K), log_marginal, {'dL_dK':dL_dK, 'dL_dthetaL':dL_dthetaL, 'dL_dm':alpha}
 
     def LOO(self, kern, X, Y, likelihood, posterior, Y_metadata=None, K=None):

GPy/kern/src/stationary.py
@@ -11,7 +11,6 @@ from ... import util
 from ...util.config import config # for assesing whether to use cython
 from paramz.caching import Cache_this
 from paramz.transformations import Logexp
-import inspect
 
 try:
     from . import stationary_cython
@@ -180,7 +179,7 @@ class Stationary(Kern):
         dist2 = dist**2
         invlengthscale = 1/self.lengthscale
         I = (np.ones((X.shape[0], X2.shape[0], X.shape[1], X2.shape[1]))*np.eye((X.shape[1]))).swapaxes(0,2).swapaxes(1,3)
-        return rinv3[None,:,:]*dist*dist2.sum(axis=0)*(invlengthscale[:,None,None]**5) -2.0*dist*rinv[None,:,:]*(invlengthscale[:,None,None]**3) if (not self.ARD) else rinv3[None,None,:,:]*dist[:,None,:,:]*(invlengthscale[None,:,None,None]**2)*dist2[:, None,:,:]*(invlengthscale[:,None,None,None]**3) -2.0*I*rinv[None,None,:,:]*dist[None,:,:,:]*(invlengthscale[None,:,None,None]**3)
+        return rinv3[None,:,:]*dist*dist2.sum(axis=0)*(invlengthscale[:,None,None]**5) -2.0*dist*rinv[None,:,:]*(invlengthscale[:,None,None]**3) if (not self.ARD) else rinv3[None,None,:,:]*dist[None,:,:,:]*(invlengthscale[None,:,None,None]**2)*dist2[:,None,:,:]*(invlengthscale[:,None,None,None]**3) -2.0*I*rinv[None,None,:,:]*dist[None,:,:,:]*(invlengthscale[None,:,None,None]**3)
 
     def dr2_dlengthscaledX2(self, X, X2):
         return -1.0*self.dr2_dlengthscaledX(X, X2)
@@ -668,7 +667,7 @@ class Stationary(Kern):
             xdi = np.nonzero(Xdi.T.reshape(-1))[0]
         if X2di is not None:
             x2di = np.nonzero(X2di.T.reshape(-1))[0]
-        
+
         #Update variance gradient
         self.variance.gradient = np.sum(dL_dK[:X.shape[0],:X2.shape[0]]*self.dK_dvariance(self._scaled_dist(X, X2)))
         if X2d is not None:
@@ -683,7 +682,7 @@ class Stationary(Kern):
             self.lengthscale.gradient = np.sum(dL_dK[:X.shape[0],:X2.shape[0]]*self.dK_dlengthscale(X, X2))
             if X2d is not None:
                 self.lengthscale.gradient += np.sum(dL_dK[None,:X.shape[0],X2.shape[0]:]*((self.dK2_dlengthscaledX2(X,X2)).swapaxes(0,1).reshape((X.shape[0],-1)))[:,x2di])
-            #if Xd is not None:
+            if Xd is not None:
                 self.lengthscale.gradient += np.sum(dL_dK[None,X.shape[0]:,:X2.shape[0]]*((self.dK2_dlengthscaledX(X,X2)).reshape((-1,X2.shape[0])))[xdi,:])
                 if X2d is not None:
                     self.lengthscale.gradient += np.sum(dL_dK[None,None,X.shape[0]:,X2.shape[0]:]*((self.dK3_dlengthscaledXdX2(X, X2)).swapaxes(1,2).reshape((Xd.shape[0]*X.shape[1], X2d.shape[0]*X2.shape[1])))[xdi,:][:, x2di])
@@ -695,8 +694,6 @@ class Stationary(Kern):
                 self.lengthscale.gradient += np.array([np.sum(dL_dK[None,None,X.shape[0]:,:X2.shape[0]]*((self.dK2_dlengthscaledX(Xd,X2)[q,:,:,:]).reshape((-1,X2.shape[0])))[xdi,:]) for q in range(0, X.shape[1])])
                 if X2d is not None:
                     self.lengthscale.gradient += np.array([np.sum(dL_dK[None,None,None,X.shape[0]:,X2.shape[0]:]*((self.dK3_dlengthscaledXdX2(Xd, X2d)[q,:,:,:,:]).swapaxes(1,2).reshape((Xd.shape[0]*X.shape[1], X2d.shape[0]*X2.shape[1])))[xdi,:][:, x2di]) for q in range(0, X.shape[1])])
-        #print(self.lengthscale.gradient)
-        #print(self.variance.gradient)
         return
     
     def update_gradients_diag(self, dL_dKdiag, X, Xd=None, Xdi=None):